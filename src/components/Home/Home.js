ba966a8ba849d65c8a1be9193ea7ecb01cd97c3f
unknown <paikpawan18@gmail.com>
2024-01-26 10:55:29 +0530

    changes some style of the text and design color

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,8 +15,8 @@ function Home() {
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
                 Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
+                <span  role="img" aria-labelledby="wave">
+                🙏 
                 </span>
               </h1>
 
